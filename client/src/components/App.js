837f26c8b8a9e0b1762905c82022906eba6b30d0
SEAN MCLIMANS <seanmclimans@gmail.com>
2018-01-19 16:07:07 -0700

    setup router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const Landing = () => <h2>Landing</h2>;
+const PostComment = () => <h2>PostComment</h2>;
 
 const App = () => {
   return <div>Maximum Hamilton, Yeah</div>;
