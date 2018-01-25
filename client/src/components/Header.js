50bed64b13a977dd540f696f985b566b36d7ecc4
SEAN MCLIMANS <seanmclimans@gmail.com>
2018-01-24 17:02:35 -0700

    render based on login

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Jumbotron } from 'react-bootstrap';
 
 class Header extends Component {
+  renderContent() {
+    console.log(this.props.auth);
+    switch (this.props.auth) {
+      case null:
+        return 'Still deciding';
+      case false:
+        return (
+          <a className="btn btn-block btn-social btn-twitter">
+            <span className="fa fa-twitter" />
+            Sign in with Twitter
+          </a>
+        );
+      default:
+        return <div>{this.props.auth.twitterId.toString()} Logged in</div>;
+    }
+  }
+
   render() {
     return (
       <Jumbotron>
-        <h1>Header</h1>
+        <h1>ComicSnob</h1>
         <p>Just some text for now</p>
-        <a className="btn btn-block btn-social btn-twitter">
-          <span className="fa fa-twitter" />
-          Sign in with Twitter
-        </a>
+        {this.renderContent()}
       </Jumbotron>
     );
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
